9169979d086dabd3fa77470472beac310fc84d9b
Talisson-b <talissonbarbosam@gmail.com>
2023-11-18 15:37:02 -0300

    feat: add initial search trip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 
-import { signIn, signOut, useSession } from "next-auth/react";
+import TripSearch from "./components/TripSearch";
 
 export default function Home() {
-  const { data } = useSession();
-
-  return <div></div>;
+  return (
+    <div>
+      <TripSearch />
+    </div>
+  );
 }
