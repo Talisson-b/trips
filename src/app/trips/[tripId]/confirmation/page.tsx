735c0b5ed3048c9d9a197d0b6086cf48ed169f79
Talisson-b <talissonbarbosam@gmail.com>
2023-11-26 01:11:35 -0300

    style: adapt trip confirmation page to desktop

--- src/app/trips/[tripId]/confirmation/page.tsx
+++ src/app/trips/[tripId]/confirmation/page.tsx
@@ -84,7 +84,7 @@ const TripConfirmation = ({ params }: { params: { tripId: string } }) => {
   const guests = searchParams.get("guests");
 
   return (
-    <div className="container mx-auto p-5 ">
+    <div className="container mx-auto p-5 lg:max-w-2xl">
       <h1 className="font-semibold text-xl text-primaryDarker">Sua viagem</h1>
       {/* CARD */}
       <div className="flex flex-col p-5 mt-5 border border-solid border-grayLighter rounded-lg shadow-lg">
